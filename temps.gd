513f6fb324295aad345a4376fe555c478e1ffcbe
rosell22 <79510212+rosell22@users.noreply.github.com>
2021-04-27 12:28:32 +0200

    hokey cronometre

--- temps.gd
+++ temps.gd
@@ -1,16 +1,17 @@
-extends Node
+extends CanvasLayer
 
 
-var temps=120
-var punts1=0
-var punts2=0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func update_time():
-	get_tree().get_nodes_in_group('temps')[0].text=String(temps)
+
